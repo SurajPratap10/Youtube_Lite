91b441570863114ef8396704b0ed5352e0e49f99
Suraj Pratap <92919173+SurajPratap10@users.noreply.github.com>
2023-01-31 17:30:30 +0530

    Add files via upload

---
+++ src/context/contextApi.js
@@ -0,0 +1,92 @@
+import React, { createContext, useState, useEffect } from "react";
+
+import { fetchDataFromApi } from "../utils/api";
+export const Context = createContext();
+
+export const AppContext = (props) => {
+    const [loading, setLoading] = useState(false);
+    const [searchResults, setSearchResults] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState("New");
+    const [mobileMenu, setMobileMenu] = useState(false);
+
+    useEffect(() => {
+        fetchSelectedCategoryData(selectedCategory);
+    }, [selectedCategory]);
+
+    const fetchSelectedCategoryData = (query) => {
+        setLoading(true);
+        fetchDataFromApi(`search/?q=${query}`).then(({ contents }) => {
+            console.log(contents);
+            setSearchResults(contents);
+            setLoading(false);
+        });
+    };
+
+    return (
+        <Context.Provider
+            value={{
+                loading,
+                setLoading,
+                searchResults,
+                selectedCategory,
+                setSelectedCategory,
+                mobileMenu,
+                setMobileMenu,
+            }}
+        >
+            {props.children}
+        </Context.Provider>
+    );
+};
+
+
+
+
+
+
+
+// import React, {createContext, useState, useEffect} from "react";
+// import { fetchDataFromApi } from "../utils/api";
+
+// export const Context = createContext();
+
+
+// //Creating States
+
+//  export const AppContext = (props) => {
+//     const [loading, setLoading] = useState(false);
+//     const [searchResults, setSearchResults] = useState(false);
+//     const [selectCategories, setSelectCategories] = useState(false);
+//     const [mobilemenu, setMobilemenu] = useState(false);
+
+//     useEffect(() => {
+
+//         fetchSelectedCategoryData (selectCategories)        
+//     }, [selectCategories]);
+
+//     const fetchSelectedCategoryData = (query) => {
+//         setLoading(true);
+//         fetchDataFromApi(`search/?q=${query}`).then(({ contents }) => {
+//             console.log(contents);
+//             setSearchResults(contents);
+//             setLoading(false);
+//         });
+//     };
+
+
+
+//     return(
+//         <Context.Provider value={{
+//             loading,
+//             setLoading,
+//             searchResults,
+//             selectCategories,
+//             setSelectCategories,
+//             mobilemenu,
+//             setMobilemenu
+//         }}>
+//             {props.children}
+//         </Context.Provider>
+//     );
+// };
+
---
+++ src/shared/loader.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Loader = () => {
+    return (
+        <div class="load-bar">
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+        </div>
+    );
+};
+
+export default Loader;
